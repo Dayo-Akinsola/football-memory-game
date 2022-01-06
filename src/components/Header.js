1fd7c31c446e8b4d3ddf49b9eb8e548f74f18d72
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-01-06 15:51:05 +0000

    added game instructions

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import Modes from './Modes';
 const Header = ({modes, changeMode}) => {
     return(
         <div className="header">
-            <h2 className="header__heading">Premier League Memory</h2>
+            <h1 className="header__heading">Premier League Memory</h1>
+            <span className="header__instructions">Click All The Cards <u>Once</u> To Win!</span>
             <Modes modes={modes} changeMode={changeMode} />
         </div>
     )
